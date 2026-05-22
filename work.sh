c13cb400f6b4265ead64fc3e1a02e53524b99ffc
Chris Ahlstrom <ahlstromcj@gmail.com>
2026-05-22 06:36:03 -0400

    Updating the build scripts.

--- work.sh
+++ work.sh
@@ -8,7 +8,7 @@
 # \library        xml66
 # \author         Chris Ahlstrom
 # \date           2025-01-30
-# \update         2026-05-21
+# \update         2026-05-22
 # \version        $Revision$
 # \license        $XPC_SUITE_GPL_LICENSE$
 #
@@ -33,7 +33,7 @@ LANG=C
 export LANG
 CYGWIN=binmode
 export CYGWIN
-export XML66_SCRIPT_EDIT_DATE="2026-05-21"
+export XML66_SCRIPT_EDIT_DATE="2026-05-22"
 export XML66_LIBRARY_API_VERSION="0.1"
 export XML66_LIBRARY_VERSION="$XML66_LIBRARY_API_VERSION.0"
 export XML66="xml66"
@@ -347,16 +347,11 @@ clean_build () {
 
    rm -f doc/dox/*.log
    rm -f doc/latex/*.log
-   echo "Build products removed from the seq66/build sub-directories."
+   echo "Build products removed from the $XML66/build sub-directories."
    rm -rf subprojects/liblib66/
    rm -rf subprojects/libcfg66/
    rm -rf subprojects/potext/          # available, but code not prep'ed
    echo "Subproject downloaded libraries removed from 'subprojects'."
-
-#  git checkout data/share/doc/seq66-dev-manual.pdf
-#  echo "Previous version of developer guide restored."
-#  Problematic when making a release. Just remember to do it.
-
 }
 
 make_pack () {
@@ -467,7 +462,7 @@ install_project () {
    USERID=$(id -u)
    if test "$USERID" = 0 ; then
       cd $BUILD_DIR
-      echo "Installing the seq66 library..."
+      echo "Installing the $XML66 library..."
       meson install
       cd ..
    else
